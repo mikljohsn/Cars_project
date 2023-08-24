be5ed8e1af4b726b2f6a24c39d78007fa8b56bc6
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-08-24 14:18:37 +0200

    Added column constraints in Member.java

--- src/main/java/dat3/car/entity/Member.java
+++ src/main/java/dat3/car/entity/Member.java
@@ -15,13 +15,21 @@ import java.time.LocalDateTime;
 public class Member {
     @Id
     private int id;
+    @Column(length = 12, nullable = false, unique = true)
     private String username;
+    @Column(length = 25, nullable = false, unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
+    @Column(length = 50, nullable = false)
     private String firstName;
+    @Column(length = 50, nullable = false)
     private String lastName;
+    @Column(length = 50)
     private String street;
+    @Column(length = 50)
     private String city;
+    @Column(length = 25)
     private String zip;
     private boolean approved;
     private int ranking;
