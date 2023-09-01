330048b3b6940d8a695cee7a1cb8f7afe2771f79
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-09-01 13:29:25 +0200

    ForFun test2

--- src/main/java/dat3/car/api/IndexController.java
+++
@@ -1,9 +0,0 @@
-package dat3.car.api;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/")
-public class IndexController {
-}
