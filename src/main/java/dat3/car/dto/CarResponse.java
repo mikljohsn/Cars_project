fb9256c30f1620a84bd97ac1e6ccfd9d894f5420
mikljohsn <114925102+mikljohsn@users.noreply.github.com>
2023-09-20 11:14:51 +0200

    Update CarResponse.java
    Added created and edited to carResponse

--- src/main/java/dat3/car/dto/CarResponse.java
+++ src/main/java/dat3/car/dto/CarResponse.java
@@ -1,8 +1,11 @@
 package dat3.car.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import dat3.car.entity.Car;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -16,6 +19,10 @@ public class CarResponse {
     private String model;
     private double pricePrDay;
     private Integer bestDiscount;
+    @JsonFormat(pattern = "dd-MM-yyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    LocalDateTime created;
+    @JsonFormat(pattern = "dd-MM-yyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+    LocalDateTime edited;
 
     public CarResponse(Car c, boolean includeAll){
         this.brand = c.getBrand();
@@ -24,6 +31,8 @@ public class CarResponse {
         if (includeAll){
             this.id = c.getId();
             this.bestDiscount = c.getBestDiscount();
+            this.created = c.getCreated();
+            this.edited = c.getEdited();
         }
     }
 }
